272738024e7b4ed95701511f250b25db039960db
Shannon Haughton <shannon.l.haughton@gsk.com>
2025-09-04 14:31:25 +0100

    update chunk test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -9,7 +9,8 @@ test_that("markdown chunks are created correctly",{
   skip_if_not(interactive())
 
   png_obj <- png_path(path = system.file("extdata/test_image.png", package = "docorator"))
-  chunk_png <- create_chunk(x = png_obj, fig_dim = c(5,8), transform = NULL) |> capture.output()
+  docorator <- as_docorator(png_obj, save_object = FALSE)
+  chunk_png <- create_chunk(x = docorator, transform = NULL) |> capture.output()
   # avoiding snapshot because of temp directory + mardown header
   expect_true(any(grepl('<div class=\"figure\">', chunk_png)))
 
@@ -21,12 +22,13 @@ test_that("markdown chunks are created correctly",{
     apply_scale(fontsize = 10,
                 tbl_scale = TRUE,
                 tbl_stub_pct = 0.5)
-
-  chunk_gt <- create_chunk(x = my_gt, fig_dim = c(5,8), transform = NULL) |> capture.output()
+  docorator <- as_docorator(my_gt, save_object = FALSE)
+  chunk_gt <- create_chunk(x = docorator, transform = NULL) |> capture.output()
   expect_snapshot(chunk_gt)
 
   my_gt_group <- gt::gt_group(my_gt,my_gt)
-  chunk_gt_group <- create_chunk(x = my_gt_group, fig_dim = c(5,8), transform = NULL) |> capture.output()
+  docorator <- as_docorator(my_gt_group, save_object = FALSE)
+  chunk_gt_group <- create_chunk(x = docorator, transform = NULL) |> capture.output()
   expect_snapshot(chunk_gt_group)
 
 })
