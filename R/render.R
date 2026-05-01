58beb1b6acce2c33baf319c6acf0f2de2bd8a104
Becca Krouse <becca.z.krouse@gsk.com>
2026-05-01 15:41:54 -0400

    update doc level font

--- R/render.R
+++ R/render.R
@@ -491,7 +491,7 @@ render_pdf_html <- function(x,
     preferCSSPageSize = TRUE,
     # TODO generalize font size and optimal positioning of page numbers
     headerTemplate = paste0(
-      '<div style="width:100%; font-size:10pt; font-family:Courier, monospace;',
+      '<div style="width:100%; font-size:10pt; font-family:Courier, \'Courier New\', monospace;',
       ' line-height:1.4; margin:0; padding:1.05in 1in 0 0; text-align:right;">',
       'Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>'
     ),
