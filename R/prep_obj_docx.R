ef3e2000ce8861f9f4d64cac4e8331239a502695
Shannon Haughton <shannon.l.haughton@gsk.com>
2026-05-22 17:37:16 +0100

    add comment and tidy

--- R/prep_obj_docx.R
+++ R/prep_obj_docx.R
@@ -22,8 +22,7 @@ prep_obj_docx <- function(x, ...) {
 #' @export
 #' @keywords internal
 prep_obj_docx.default <- function(x, ...) {
-  xml <- polish::polish_content_word(x$display)
-  xml
+  polish::polish_content_word(x$display)
 }
 
 #' @rdname prep_obj_docx
@@ -41,8 +40,7 @@ prep_obj_docx.PNG <- function(x, ...) {
 #' @export
 #' @keywords internal
 prep_obj_docx.gt_tbl <- function(x, ...) {
-  xml <- gt_to_word(x$display)
-  xml
+  gt_to_word(x$display)
 }
 
 #' @rdname prep_obj_docx
@@ -50,11 +48,10 @@ prep_obj_docx.gt_tbl <- function(x, ...) {
 #' @keywords internal
 prep_obj_docx.gt_group <- function(x, ...) {
   # list of ooxml - one for each table in the gt_group
-  xml <- lapply(x$display$gt_tbls$gt_tbl, function(i) {
+  lapply(x$display$gt_tbls$gt_tbl, function(i) {
     # new docorator object with gt_tbl as display
     gt_to_word(i)
   })
-  xml
 }
 
 #' convert gt_tbl object to ooxml
@@ -63,7 +60,9 @@ gt_to_word <- function(x) {
   # get ooxml from gt
   ooxml <- polish::polish_content_word(x, autonum = FALSE)
 
-  tbl_nodes <- paste0(
+  # as_word from gt returns multiple ooxml elements
+  # wrap these elements together so that they can be read in as xml for officer
+  paste0(
     "<tablecontainer>",
     paste0(ooxml, collapse = ""),
     "</tablecontainer>"
@@ -71,6 +70,4 @@ gt_to_word <- function(x) {
     xml2::read_xml() |>
     xml2::xml_children() |>
     suppressWarnings()
-
-  tbl_nodes
 }
