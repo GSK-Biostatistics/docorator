359ff0f232b9a430efd03bc00b2cc655607f0eec
Shannon Haughton <shannon.l.haughton@gsk.com>
2026-09-03 16:22:40 +0100

    allow the footnotes to wrap

--- R/render_pdf.R
+++ R/render_pdf.R
@@ -4,7 +4,6 @@
 #' @param display_loc optional path to save the output pdf to
 #' @param engine character vector of rendering engines to use. Options are "latex" (default) and "html".
 #' @param version_check Boolean indicating whether to print a note if gt or ggplot versions dont match between the original docorator object and the one being used for rendering
-#' @param fancywrap Boolean indicating if headers and footers should be split to fit the page. Defaults to `TRUE`. Note that only fancyrows with one `left`, `right` OR `center` element will be wrapped. `r lifecycle::badge("experimental")`
 #' @param ... Additional arguments passed to the engine-specific render function.
 #'
 #'   For `engine = "latex"`, see [render_pdf_latex()] for supported arguments.
@@ -33,7 +32,6 @@ render_pdf <- function(
   display_loc = NULL,
   engine = c("latex", "html"),
   version_check = TRUE,
-  fancywrap = TRUE,
   ...
 ) {
   if (!inherits(x, "docorator")) {
@@ -46,10 +44,6 @@ render_pdf <- function(
   # check package versions
   if (isTRUE(version_check)) {
     check_pkg_version(x)
-  }
-
-  if (isTRUE(fancywrap)) {
-    x <- fancywrap(x)
   }
 
   # get the pdf engine and switch to the appropriate render function
@@ -75,6 +69,7 @@ render_pdf <- function(
 #'
 #' @param x `docorator` object
 #' @param display_loc optional path to save the output pdf to
+#' @param fancywrap Boolean indicating whether to wrap the header and footer text in a LaTeX \fbox{} command. Defaults to TRUE. - valid for latex engine only
 #' @param transform optional latex transformation function to apply to a gt latex string - valid for latex engine only
 #' @param header_latex optional .tex file of header latex - valid for latex engine only
 #' @param keep_tex Boolean indicating if to keep resulting .tex file from latex conversion. Defaults to FALSE. - valid for latex engine only
@@ -100,6 +95,7 @@ render_pdf <- function(
 render_pdf_latex <- function(
   x,
   display_loc = NULL,
+  fancywrap = TRUE,
   transform = NULL,
   header_latex = NULL,
   keep_tex = FALSE,
@@ -114,6 +110,11 @@ render_pdf_latex <- function(
       call = rlang::caller_env()
     )
     transform <- NULL
+  }
+
+  # wrap headers and footers if fancywrap is TRUE
+  if (isTRUE(fancywrap)) {
+    x <- fancywrap(x)
   }
 
   # copy template rmd to a temp directory
